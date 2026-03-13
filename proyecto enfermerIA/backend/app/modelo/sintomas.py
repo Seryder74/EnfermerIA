59aa3106b2f0a5ec172adb2f534d7efcfa4ad506
Alexis <alexis12acua@gmail.com>
2026-03-13 09:28:16 -0600

    añadido las funciones de react

--- proyecto enfermerIA/backend/app/modelo/sintomas.py
+++
@@ -1,8 +0,0 @@
-class Sintomas:
-    def __init__(self, respira=True, consciente=True, sangrado=False,
-                convulsion=False, quemadura_grave=False):
-        self.respira = respira
-        self.consciente = consciente
-        self.sangrado = sangrado
-        self.convulsion = convulsion
-        self.quemadura_grave = quemadura_grave
